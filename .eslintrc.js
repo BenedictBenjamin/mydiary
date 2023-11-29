8c10eb68ce38668b1f2c74f99db8dde44f14e913
BenedictBenjamin <siwoo98511@gmail.com>
2023-11-29 17:02:43 +0900

    feat : 기본 레이어아웃 구성 진행

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended"
-        ,"prettierrc"
+        ,"prettier"
     ],
     "overrides": [
         {
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import { BotBar } from "../styles/footerstyle";
+
+const Footer = () => {
+  return <BotBar>하단영역</BotBar>;
+};
+
+export default Footer;
